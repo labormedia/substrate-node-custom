c5a5c4986eb661d4c393f70c16d75cdc6ee9fd67
Diego Correa T <algoritmia@labormedia.cl>
2022-09-24 02:02:49 +0000

    First solution approach

--- pallets/template/src/lib.rs
+++ pallets/template/src/lib.rs
@@ -16,7 +16,7 @@ mod benchmarking;
 
 #[frame_support::pallet]
 pub mod pallet {
-	use frame_support::pallet_prelude::*;
+	use frame_support::{pallet_prelude::*, debug};
 	use frame_system::pallet_prelude::*;
 
 	#[pallet::pallet]
@@ -38,6 +38,9 @@ pub mod pallet {
 	// https://docs.substrate.io/main-docs/build/runtime-storage/#declaring-storage-items
 	pub type Something<T> = StorageValue<_, u32>;
 
+	#[pallet::storage]
+	pub type Members<T: Config> = StorageMap<_, Blake2_128Concat, T::AccountId, T::AccountId, OptionQuery>;
+
 	// Pallets use events to inform users when important changes are made.
 	// https://docs.substrate.io/main-docs/build/events-errors/
 	#[pallet::event]
@@ -46,6 +49,10 @@ pub mod pallet {
 		/// Event documentation should end with an array that provides descriptive names for event
 		/// parameters. [something, who]
 		SomethingStored(u32, T::AccountId),
+		/// [who, candidate]
+		MemberStored(T::AccountId, T::AccountId),
+		MemberRemoved(T::AccountId, T::AccountId),
+		MemberEnumerated(T::AccountId)
 	}
 
 	// Errors inform users that something went wrong.
@@ -62,6 +69,59 @@ pub mod pallet {
 	// Dispatchable functions must be annotated with a weight and must return a DispatchResult.
 	#[pallet::call]
 	impl<T: Config> Pallet<T> {
+		/// A dispatchable that emits an event with Members Storage enumeration. This is just for testing purposes and it is not considered efficient to implement on every node.
+		///  This function must be dispatched by a signed extrinsic.
+		#[pallet::weight(10_000 + T::DbWeight::get().writes(1))]
+		pub fn enumerate_members(origin: OriginFor<T>) -> DispatchResultWithPostInfo {
+			// Check that the extrinsic was signed and get the signer.
+			// This function will return an error if the extrinsic is not signed.
+			// https://docs.substrate.io/v3/runtime/origins
+			let _who = ensure_signed(origin)?;
+
+			// Update storage.
+			for x in Members::<T>::iter_keys() {
+				debug(&x);
+			};
+			
+			// Emit an event.
+			// Self::deposit_event(Event::MemberRemoved(candidate, who));
+			// Return a successful DispatchResultWithPostInfo
+			Ok(().into())
+		}
+		/// A dispatchable that takes An accountId as a parameter, removes the value from
+		/// storage and emits an event. This function must be dispatched by a signed extrinsic.
+		#[pallet::weight(10_000 + T::DbWeight::get().writes(1))]
+		pub fn remove_member(origin: OriginFor<T>, candidate: T::AccountId) -> DispatchResultWithPostInfo {
+			// Check that the extrinsic was signed and get the signer.
+			// This function will return an error if the extrinsic is not signed.
+			// https://docs.substrate.io/v3/runtime/origins
+			let who = ensure_signed(origin)?;
+
+			// Update storage.
+			<Members<T>>::remove(candidate.clone());
+
+			// Emit an event.
+			Self::deposit_event(Event::MemberRemoved(candidate, who));
+			// Return a successful DispatchResultWithPostInfo
+			Ok(().into())
+		}
+		/// A dispatchable that takes An accountId as a parameter, writes the value to
+		/// storage and emits an event. This function must be dispatched by a signed extrinsic.
+		#[pallet::weight(10_000 + T::DbWeight::get().writes(1))]
+		pub fn set_member(origin: OriginFor<T>, candidate: T::AccountId) -> DispatchResultWithPostInfo {
+			// Check that the extrinsic was signed and get the signer.
+			// This function will return an error if the extrinsic is not signed.
+			// https://docs.substrate.io/v3/runtime/origins
+			let who = ensure_signed(origin)?;
+
+			// Update storage.
+			<Members<T>>::set(candidate.clone(), Some(who.clone()));
+
+			// Emit an event.
+			Self::deposit_event(Event::MemberStored(candidate, who));
+			// Return a successful DispatchResultWithPostInfo
+			Ok(().into())
+		}
 		/// An example dispatchable that takes a singles value as a parameter, writes the value to
 		/// storage and emits an event. This function must be dispatched by a signed extrinsic.
 		#[pallet::weight(10_000 + T::DbWeight::get().writes(1))]
