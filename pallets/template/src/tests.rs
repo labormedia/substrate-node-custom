4b1e66fae902eae067418832996f2479034f1142
Diego Correa T <algoritmia@labormedia.cl>
2022-10-17 19:10:50 +0000

    valid unit tests

--- pallets/template/src/tests.rs
+++ pallets/template/src/tests.rs
@@ -6,7 +6,7 @@ use frame_support::{assert_noop, assert_ok};
 fn it_works_for_default_value() {
 	new_test_ext().execute_with(|| {
 		// Dispatch a signed extrinsic.
-		assert_ok!(TemplateModule::set_member(mock::Origin::root(), 42, 43));
+		assert_ok!(TemplateModule::set_member(mock::Origin::signed(1), 42, 43));
 		// Read pallet storage and assert an expected result.
 		frame_support::debug(&TemplateModule::check(42, 43));
 		assert_eq!(TemplateModule::check(42, 43), Some(1));
@@ -17,6 +17,6 @@ fn it_works_for_default_value() {
 fn correct_error_for_none_value() {
 	new_test_ext().execute_with(|| {
 		// Ensure the expected error is thrown when no value is present.
-		assert_noop!(TemplateModule::cause_error(mock::Origin::root(), 42, 43), Error::<Test>::NoneValue);
+		assert_noop!(TemplateModule::cause_error(mock::Origin::signed(1), 42, 43), Error::<Test>::NoneValue);
 	});
 }
